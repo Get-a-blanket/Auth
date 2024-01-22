4acfac85826aa317fdb7e92fa57c00a4753dea2b
Николай Перминов <kolya-perminov@ya.ru>
2024-01-22 23:45:20 +0500

    Добавьте файлы проекта.

---
+++ Gab_Auth/ProtectedDbConnector/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using GaB_Auth.DbConnector.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaB_Auth.DbConnector;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(GaB_Auth.ConfigurationManager.ConnectionStringProtectedDb);
+    }
+}
